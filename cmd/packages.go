05f1306c31f07821002adc4d986b13c188bdb8a5
Francisco Daines <francisco.daines@walmart.com>
2021-02-24 22:49:49 -0300

    Includes console output format

--- cmd/packages.go
+++ cmd/packages.go
@@ -8,6 +8,8 @@ import (
 	"github.com/fdaines/spm-go/utils"
 	"github.com/spf13/cobra"
 	"go/build"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"os"
 )
 
 var packagesCmd = &cobra.Command{
@@ -68,7 +70,18 @@ func printPackages(packages []*model.PackageInfo, format string) {
 			fmt.Printf("%s;%d\n", p.Path, p.FilesCount)
 		}
 	} else if format == "console" {
-		fmt.Println("Output in 'console' format is not implemented.")
+		index := 1
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{"#", "Package", "Files Count"})
+
+		for _, pkg := range packages {
+			t.AppendRows([]table.Row{
+				{index, pkg.Path, pkg.FilesCount},
+			})
+			index = index + 1
+		}
+		t.Render()
 	} else if format == "json" {
 		summary := &model.PackagesSummary{
 			Packages: packages,
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/fdaines/spm-go
 go 1.15
 
 require (
+	github.com/jedib0t/go-pretty/v6 v6.1.0
 	github.com/spf13/cobra v1.1.3
 	golang.org/x/tools v0.1.0
 )
