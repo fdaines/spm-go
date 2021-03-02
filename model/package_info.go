f2e64d11da3b1e0901364fdf986356a98e50f1b8
Francisco Daines <francisco.daines@walmart.com>
2021-03-01 23:06:38 -0300

    exclude empty values from json results structure

--- model/package_info.go
+++ model/package_info.go
@@ -3,13 +3,13 @@ package model
 type PackageInfo struct {
 	Name string `json:"name"`
 	Path string `json:"path"`
-	Files []string `json:"files"`
-	FilesCount int `json:"files_count"`
-	Dependencies *DependenciesInfo `json:"dependencies"`
-	Dependants []string `json:"dependants"`
-	AfferentCoupling int `json:"afferent_coupling"`
-	EfferentCoupling int `json:"efferent_coupling"`
-	Instability float64 `json:"instability"`
+	Files []string `json:"files,omitempty"`
+	FilesCount int `json:"files_count,omitempty"`
+	Dependencies *DependenciesInfo `json:"dependencies,omitempty"`
+	Dependants []string `json:"dependants,omitempty"`
+	AfferentCoupling int `json:"afferent_coupling,omitempty"`
+	EfferentCoupling int `json:"efferent_coupling,omitempty"`
+	Instability float64 `json:"instability,omitempty"`
 }
 
 type PackagesSummary struct {
@@ -17,11 +17,11 @@ type PackagesSummary struct {
 }
 
 type DependenciesInfo struct {
-	Standard []string `json:"standard"`
-	Internals []string `json:"internals"`
-	Externals []string `json:"externals"`
-	StandardCount int `json:"standard_count"`
-	InternalsCount int `json:"internals_count"`
-	ExternalsCount int `json:"externals_count"`
-	TotalCount int `json:"count"`
+	Standard []string `json:"standard,omitempty"`
+	Internals []string `json:"internals,omitempty"`
+	Externals []string `json:"externals,omitempty"`
+	StandardCount int `json:"standard_count,omitempty"`
+	InternalsCount int `json:"internals_count,omitempty"`
+	ExternalsCount int `json:"externals_count,omitempty"`
+	TotalCount int `json:"count,omitempty"`
 }
