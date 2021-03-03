372dcab057062c70a0ebe9283e44d8b160c32800
Francisco Daines <francisco.daines@walmart.com>
2021-03-03 15:41:25 -0300

    Implements 'all' command

---
+++ cmd/all.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"github.com/fdaines/spm-go/cmd/all"
+	"github.com/fdaines/spm-go/cmd/dependencies"
+	"github.com/fdaines/spm-go/cmd/packages"
+	"github.com/fdaines/spm-go/common"
+	"github.com/fdaines/spm-go/utils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	allCmd = &cobra.Command{
+		Use:   "all",
+		Short: "Displays all metric for packages",
+		Args:  ValidateArgs,
+		Run:   listAllMetrics,
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(allCmd)
+}
+
+func listAllMetrics(cmd *cobra.Command, args []string) {
+	utils.ExecuteWithTimer(func() {
+		utils.PrintMessage("Full analysis started.")
+		var afferentMap = make(map[string][]string)
+		pkgsInfo := getBasicPackagesInfo()
+		utils.PrintMessage("Gathering package metrics, please wait until the command is finished running.")
+		for index, pkgInfo := range pkgsInfo {
+			pkg, err := context.Import(pkgInfo.Path, "", 0)
+			if err == nil {
+				pkgsInfo[index] = packages.FillFiles(pkgInfo, pkg)
+				pkgsInfo[index] = dependencies.FillDependencies(pkgsInfo[index], pkg, pkgsInfo)
+				for _, current := range pkgsInfo[index].Dependencies.Internals {
+					afferentMap[current] = append(afferentMap[pkgInfo.Path], current)
+				}
+				abstractnessInfo, _ := retrieveAbstractnessInfo(pkg)
+				pkgsInfo[index].AbstractnessDetails = abstractnessInfo
+				pkgsInfo[index].AbstractionsCount = abstractnessInfo.StructsCount + abstractnessInfo.InterfacesCount
+				pkgsInfo[index].ImplementationsCount = abstractnessInfo.MethodsCount + abstractnessInfo.FunctionsCount
+				pkgsInfo[index].Abstractness = calculateAbstractness(pkgsInfo[index].AbstractionsCount, pkgsInfo[index].ImplementationsCount)
+			}
+		}
+		for index, pkgInfo := range pkgsInfo {
+			pkgsInfo[index].Dependants = afferentMap[pkgInfo.Path]
+			pkgsInfo[index].AfferentCoupling = len(pkgsInfo[index].Dependants)
+			pkgsInfo[index].EfferentCoupling = pkgsInfo[index].Dependencies.InternalsCount
+			pkgsInfo[index].Instability = calculateInstability(pkgsInfo[index])
+			pkgsInfo[index].Distance = calculateDistance(pkgsInfo[index].Instability, pkgsInfo[index].Abstractness)
+		}
+		all.PrintPackages(pkgsInfo, common.OutputFormat)
+	})
+}
---
+++ cmd/all/output.go
@@ -0,0 +1,72 @@
+package all
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/fdaines/spm-go/model"
+	"github.com/fdaines/spm-go/utils"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"os"
+)
+
+func PrintPackages(packages []*model.PackageInfo, format string) {
+	if format == "csv" {
+		utils.PrintMessage("Package;Files;Afferent;Efferent;Abstractions;Implementations;Instability;Abstractness;Distance\n")
+		for _, p := range packages {
+			utils.PrintMessage(fmt.Sprintf(
+				"%s;%d;%d;%d;%.2f;%.2f;%.2f\n",
+				p.Path,
+				p.FilesCount,
+				p.AfferentCoupling,
+				p.EfferentCoupling,
+				p.Instability,
+				p.Abstractness,
+				p.Distance,
+			))
+		}
+	} else if format == "console" {
+		index := 1
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{
+			"#",
+			"Package",
+			"Files",
+			"Afferent",
+			"Efferent",
+			"Abstractions",
+			"Implementations",
+			"Instability",
+			"Abstractness",
+			"Distance",
+		})
+
+		for _, pkg := range packages {
+			t.AppendRows([]table.Row{
+				{
+					index,
+					pkg.Path,
+					pkg.FilesCount,
+					pkg.AfferentCoupling,
+					pkg.EfferentCoupling,
+					pkg.AbstractionsCount,
+					pkg.ImplementationsCount,
+					pkg.Instability,
+					pkg.Abstractness,
+					pkg.Distance,
+				},
+			})
+			index = index + 1
+		}
+		t.Render()
+	} else if format == "json" {
+		summary := &model.PackagesSummary{
+			Packages: packages,
+		}
+		b, err := json.MarshalIndent(summary, "", "    ")
+		if err != nil {
+			utils.PrintMessage(fmt.Sprintf("%v\n", err))
+		}
+		utils.PrintMessage(fmt.Sprintf("%s\n", string(b)))
+	}
+}
